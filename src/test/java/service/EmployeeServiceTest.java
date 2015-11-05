98bbddc5748d31736ec555c88f27c2313898003b
Yonggao <ygdong@thoughtworks.com>
2015-11-05 11:59:15 +0800

    add ut

---
+++ src/test/java/service/EmployeeServiceTest.java
@@ -0,0 +1,43 @@
+package service;
+
+import org.junit.BeforeClass;
+import org.testng.annotations.Test;
+import vo.Employee;
+
+/**
+ * Created by Yonggao.Dong on 15/11/5.
+ */
+public class EmployeeServiceTest {
+    EmployeeService service ;
+
+    @BeforeClass
+    public void init(){
+        service = new EmployeeService();
+    }
+
+
+    @Test
+    public void testAdd() throws Exception {
+        service.add("name",19);
+    }
+
+    @Test
+    public void testAdd1() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        Employee em = service.get("1");
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testGet1() throws Exception {
+
+    }
+}
